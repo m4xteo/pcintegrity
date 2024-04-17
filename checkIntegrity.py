7258581ff04cf3dcdfb8e1c687616458d3de4d89
Matt <117918420+m4xteo@users.noreply.github.com>
2024-04-17 21:11:11 +1200

    Create checkIntegrity.py

---
+++ checkIntegrity.py
@@ -0,0 +1,34 @@
+'''
+
+┏┓         ┳         •   
+┃ ┏┓┓┏┏┓╋┏┓┃┏┓╋┏┓┏┓┏┓┓╋┓┏
+┗┛┛ ┗┫┣┛┗┗┛┻┛┗┗┗ ┗┫┛ ┗┗┗┫
+     ┛┛           ┛     ┛
+# Check your computer if its safe to transact Banking / Crypto Transactions by detecting multiple and common methods
+of attack.
+
+# Developed by M4XTEO
+'''
+
+checkCount = []
+
+
+def main():
+	performChecks()
+
+def performChecks():
+	checkClipboard()
+
+# Checks clipboard attacks commonly used for swapping crypto out.
+def checkClipboard():
+	return x
+
+# Check Network Integrity by performing common checks
+def checkNetwork():
+	return x
+
+# Perform browser checks
+def checkBrowsers():
+
+# Perform system checks 
+def checkSystem():
